eb4364dbf4228cd51a03a875516cf12605b0ceb5
hyungjunelee <hyungjune.lee@gmail.com>
2015-03-15 23:06:59 +0900

    Create ewha_ssp_hw1_1.cpp
    For GitHub setup homework

---
+++ ewha_ssp_hw1_1.cpp
@@ -0,0 +1,24 @@
+/*
+ * ewha_ssp_hw1_1.cpp
+ *
+ * 37269-01 Smart Software Project
+ * GitHub Setup Homework 
+ * Instructor: Prof. HyungJune Lee
+ *
+ * Coded by Firstname Lastname
+ * Mar xx, 2015
+ *
+ */
+
+// Add #define pre-processor below
+
+ 
+void setup() {
+  //Add your code of Lab Assignment #1
+
+}
+
+void loop() {
+  //Add your code of Lab Assignment #1  
+
+}
